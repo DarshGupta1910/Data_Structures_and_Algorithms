cf4546ec2df9911b4076274efc3b38c0c666eea8
DarshGupta1910 <73016212+DarshGupta1910@users.noreply.github.com>
2021-06-12 00:32:14 +0530

    Create Pure Virtual Function.cpp

---
+++ Essential C++ Concepts/Polymorphism/Pure Virtual Function.cpp
@@ -0,0 +1,78 @@
+/*  C++ Program to illustrate the use of &nbsp;Pure virtual function  */
+
+#include<iostream>
+using namespace std;
+class Shape   // Abstract Base Class
+{
+                public:
+                        virtual void Enter_data( ) = 0;  // pure virtual function
+                        virtual void Area( ) = 0 ;            // pure virtual function
+};
+class Rectangle : public Shape // First Derived class
+{
+                private:
+                        float length ;
+                        float breadth ;
+                
+                public:
+                        void Enter_data(void)
+                        {
+                                cout << "\n Enter the data for the Rectangle........" ;
+                                cout << "\n\t Enter the length of the rectangle: " ;
+                                cin >> length ;
+                                cout << "\t Enter the breadth of the rectangle: " ;
+                                cin >> breadth ;
+                        }
+                        void Area(void)
+                        {
+                                cout << "\n\t The area of the rectangle = " << (length * breadth) ;
+                        }
+};
+
+class Circle : public Shape // Second Derived class
+{
+                private:
+                        float radius ;
+                public:
+                        void Enter_data(void)
+                        {
+                                cout << "\n\n Enter the data for the Circle..........." ;
+                                cout << "\n\t Enter the radius of the circle: " ;
+                                cin >> radius ;
+                        }
+                        void Area(void)
+                        {
+                                cout << "\n\t The area of the circle = " << (3.14 * radius * radius) ;
+                        }
+};
+int main()
+{
+                Shape* shp ; // pointer to the object of the base class Shape
+                Rectangle rec ; // object of class Rectangle
+                shp = &rec ;
+                shp->Enter_data( ) ;
+                shp->Area( );
+                Circle cir ;     // object of class Circle
+                shp = &cir ;
+                shp->Enter_data( );
+                shp->Area( );
+return 0;
+}
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++ OUTPUT +++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+/*  C++ Program to illustrate the use of Pure virtual function  */
+
+Enter the data for the Rectangle........
+         Enter the length of the rectangle: 5
+         Enter the breadth of the rectangle: 7
+
+         The area of the rectangle = 35
+
+ Enter the data for the Circle...........
+         Enter the radius of the circle: 3
+
+         The area of the circle = 28.26
